6fd1db0299c710dc4df4b6a7285315c24fbf4b1f
Justin Bakse <baksej@newschool.edu>
2015-08-19 00:42:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Template
+
+This is an empty project using the [p5.js](http://p5js.org/) library.
---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // uncomment this line to make the canvas the full size of the window
+  createCanvas(windowWidth, windowHeight);
+ 
+  
+}
+
+function draw() {
+  // draw stuff here
+  background(10);
+  fill(255, 255, 255);
+  stroke(255, 0, 0);
+  ellipse(50, 50, 50, 50);
+}
